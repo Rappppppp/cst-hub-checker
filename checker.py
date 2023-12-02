07c5f10e53fa11d67803b0b0ebd1c61524106250
Raphael Herreria <arolatenci@gmail.com>
2023-12-02 21:04:33 +0800

    first commit

---
+++ checker.py
@@ -0,0 +1,495 @@
+# text detection
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
+
+# image generation
+import fitz
+import numpy as np
+import cv2
+
+# this code disables display plot
+import matplotlib as mpl
+mpl.use('Agg')
+
+from matplotlib import pyplot as plt
+
+import os
+import re
+# END IMPORT #
+
+def getFontStyle(font_family):
+    if font_family == 'Times New Roman':
+        acceptable_fonts = {
+            'Times New Roman',
+            'Times New Roman,Bold',
+            'Times New Roman,Italic',
+            'TimesNewRoman',
+            'TimesNewRoman,Italic',
+            'TimesNewRoman,Bold',
+            'TimesNewRoman,BoldItalic',
+            'TimesNewRomanPS-BoldMT',
+            'TimesNewRomanPS-BoldItalicMT',
+            'TimesNewRomanPS-ItalicMT',
+            'TimesNewRomanPSMT',
+            'TimesNewRomanPSMT-Bold',
+            'Times-Bold',
+            'Times-Roman',
+            'Times-Italic'
+        }
+
+        return acceptable_fonts
+        
+    elif font_family == 'Arial':
+        arial = 'add here'
+
+    elif font_family == 'Calibri':
+        calibri = 'add calibri'
+
+# CALCULATE MARGINS AND SPACINGS
+def calculate(img, boxes):
+    # Load the image from the provided path
+
+    height, width = img.shape[:2]
+
+    if not boxes:  # Check if boxes list is empty
+        # Set default values for margins
+        top_margin = 0
+        bottom_margin = 0
+        left_margin = 0
+        right_margin = 0
+        min_x = 0
+        max_x = 0
+        min_y = 0
+        max_y = 0
+        spacings = []
+        spacing_idx = []
+    else:
+        min_x = min(box[0] for box in boxes)
+        max_x = max(box[0] + box[2] for box in boxes)
+        min_y = min(box[1] for box in boxes)
+        max_y = max(box[1] + box[3] for box in boxes)
+
+        top_margin = min_y / height
+        bottom_margin = (height - max_y) / height
+        left_margin = min_x / width
+        right_margin = (width - max_x) / width
+        spacing_idx = []
+        spacings = []
+        sorted_boxes = sorted(boxes, key=lambda box: box[1])
+        for i in range(len(sorted_boxes) - 1):
+            y_bottom = sorted_boxes[i][1] + sorted_boxes[i][3]
+            y_top = sorted_boxes[i+1][1]
+            spacing = ((y_top - y_bottom) / height) * 100
+            if spacing >= 2 and spacing <= 2.85:  # modify this if necessary
+                spacing = 2
+            if spacing >= 0.3 and spacing <= 1.85:
+                spacing = 1
+            if spacing >= 1:
+                spacings.append(spacing)
+                spacing_idx.append(i)
+
+    return top_margin, bottom_margin, left_margin, right_margin, min_x, max_x, spacings, spacing_idx # min_y, max_y, height, width : removed
+
+def margin(img, top_margin, bottom_margin, left_margin, right_margin, margins): 
+    margin_ret = []
+
+    # OPENCV LINE EXTRACTION
+    margin_height_top = int(top_margin * img.shape[0])
+    margin_height_bottom = int((1 - bottom_margin) * img.shape[0])
+    margin_width_left = int(left_margin * img.shape[1]) 
+    margin_width_right = int((1 - right_margin) * img.shape[1])
+
+    # print(margin_height_bottom)
+
+    # decode json_margins paramter from the DATABASE
+    top = float(margins['margin_top'])
+    bottom = float(margins['margin_bottom'])
+    left = float(margins['margin_left'])
+    right = float(margins['margin_right'])
+
+    margin_color = (255, 0, 0)  # RGB
+    margin_thickness = 2
+
+    # UPDATE AND ADD IF STATEMENTS HERE, MUTATE VALUES
+    top_round = round(top_margin, 2)
+    bottom_round = round(bottom_margin, 2)
+    left_round = round(left_margin, 2)
+    right_round = round(right_margin, 2)
+
+    # TOP
+    if top == 1:
+        correct_top = 300
+        margin_top = [
+            0.05, # remove if there's no number in top
+            0.09,
+            0.1
+        ]
+    
+    # LEFT
+    if left == 1:
+        correct_left = 284
+        margin_left = [
+            0.12,
+            1.0
+        ]
+
+    if left == 1.5:
+        correct_left = 432
+        margin_left = [
+            0.16,
+            0.17,
+            0.18,
+            0.19 
+        ]
+
+    # RIGHT
+    if right == 1:
+        margin_right = 2152
+        right_margin_one = [
+            0.12,
+            0.13
+        ]
+
+    # BOTTOM
+    if bottom == 1:
+        correct_bottom = 2701
+        margin_bottom = [
+            0.09,
+            0.1,
+            0.11,
+            0.13,
+            0.14,
+            0.15
+        ]
+
+    # Top margin
+    if top_round not in margin_top:
+        cv2.line(img, (0, correct_top), (img.shape[1], correct_top), (62,199,214), margin_thickness)
+        margin_ret.append({'top_margin': top_round})
+    else:
+        margin_ret.append({'top_margin': 'N/A'})
+
+    # Bottom margin
+    if bottom_round not in margin_bottom:
+        cv2.line(img, (0, correct_bottom), (img.shape[1], correct_bottom), (241,75,38), margin_thickness)
+        margin_ret.append({'bottom_margin': bottom_round})
+    else:
+        margin_ret.append({'bottom_margin': 'N/A'})
+
+    # Left margin
+    if left_round not in margin_left:
+        cv2.line(img, (correct_left, 0), (correct_left, img.shape[0]), (0, 0, 0), margin_thickness)
+        margin_ret.append({'left_margin': left_round})
+    else:
+        margin_ret.append({'left_margin': 'N/A'})
+
+    # Right margin
+    if right_round not in right_margin_one:
+        cv2.line(img, (margin_right, 0), (margin_right, img.shape[0]), (0,126,167), margin_thickness)
+        margin_ret.append({'right_margin': right_round})
+    else:
+        margin_ret.append({'right_margin': 'N/A'})
+
+    return margin_ret
+
+def spacing(img, spacings, spacing_indices, text_boxes, min_x, max_x, accepted_spacings):
+    spacing_color = (55, 183, 9) 
+    spacing_thickness = 2
+    spacings_ret = []
+
+    sorted_boxes = sorted(text_boxes, key=lambda box: box[1])
+    for i, index in enumerate(spacing_indices):
+        if index < len(sorted_boxes) - 1:
+            y_bottom = sorted_boxes[index][1] + sorted_boxes[index][3]
+            y_top = sorted_boxes[index+1][1]
+            spacing_height = int((y_bottom + y_top) / 2)
+            spaces = "{:.0f}".format(spacings[i])
+            if spaces != accepted_spacings:
+                cv2.line(img, (min_x, y_top), (max_x, y_top), spacing_color, spacing_thickness)
+                # cv2.line(img, (min_x, spacing_height), (max_x, spacing_height), spacing_color, spacing_thickness) # middle
+                cv2.line(img, (min_x, y_bottom), (max_x, y_bottom), spacing_color, spacing_thickness)
+                cv2.putText(img, spaces, (int((min_x + max_x) / 2), spacing_height + 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 2)
+                spacings_ret.append(spaces)
+                
+    return spacings_ret
+
+def remove_rn(data):
+    # regular expression pattern to match both uppercase and lowercase Roman numerals
+    roman_numeral_pattern = r'\b[MmDdCcLlXxVvIi]+\b'
+
+    # remove Roman numerals
+    for i in range(len(data['text'])):
+        data['text'][i] = re.sub(roman_numeral_pattern, '', data['text'][i]).strip()
+
+    return data
+
+def getSpecificPage(pdf_path, selection):
+    pdf_document = fitz.open(pdf_path)
+
+    n_pages = pdf_document.page_count
+
+    section_headings = {
+    'Preliminaries': [
+        'ABSTRACT',
+        'ACKNOWLEDGEMENTS',
+        'TABLE OF CONTENTS',
+        'LIST OF FIGURES',
+        'LIST OF TABLES',
+        'LIST OF ABBREVIATIONS'
+    ],
+    'Chapter 1': ['CHAPTER I'],
+    'Chapter 2': ['CHAPTER II'],
+    'Chapter 3': ['CHAPTER III'],
+    'Chapter 4': ['CHAPTER IV'],
+    'Chapter 5': ['CHAPTER V'],
+    'Bibliography' : ['BIBLIOGRAPHY']
+}
+
+    sections = {}
+
+    if selection not in section_headings:
+        return '' # selection not recognized
+
+    headings = section_headings[selection]
+    n_pages = len(pdf_document)
+    found_heading = None
+    next_heading = None
+
+    for page_num in range(n_pages):
+        page = pdf_document[page_num]
+        page_text = page.get_text()
+
+        for heading in headings:
+            pattern = rf'(?i)\b{heading}\b'
+            match = re.search(pattern, page_text)
+            if match:
+                found_heading = heading
+                sections[heading] = {
+                    'starting_page': page_num + 1,
+                }
+            # else:
+            #     return ''
+                         
+        if selection != 'Preliminaries':
+            if found_heading:
+                # if a heading has been found, check for the next heading
+                next_headings = section_headings.get(f'Chapter {int(selection.split(" ")[-1]) + 1}', [])
+                if next_headings:
+                    for next_heading in next_headings:
+                        next_pattern = rf'(?i)\b{next_heading}\b'
+                        next_match = re.search(next_pattern, page_text)
+                        if next_match:
+                            sections[found_heading]['ending_page'] = page_num
+                            return sections
+                else:
+                # if there is no next chapter, check for the 'Bibliography' section
+                    bibliography_headings = section_headings.get('Bibliography', [])
+                    for bibliography_heading in bibliography_headings:
+                        bibliography_pattern = rf'(?i)\b{bibliography_heading}\b'
+                        bibliography_match = re.search(bibliography_pattern, page_text)
+                        if bibliography_match:
+                            sections[found_heading]['ending_page'] = page_num
+                            return sections
+
+def analyzePDF(pdf_path, acceptable_fonts, accepted_spacings, margins_json, selection):
+    pdf_document = fitz.open(pdf_path)
+
+    from datetime import datetime
+    datetime = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    directory = f'temp_images/{selection}/{datetime}/'
+    os.makedirs(directory)
+
+    sections_page = getSpecificPage(pdf_document, selection)
+
+    # GAWIN KO MAMAYA!!
+    # if isinstance(sections_page, str):
+    #     return f'{selection} not recognized. Please upload a full document paper.', ''
+
+    extracted_pages = list(sections_page.values())[0]
+
+    # assign starting and ending page
+    starting_page = extracted_pages['starting_page']
+    ending_page = extracted_pages['ending_page']
+    
+    px = 4
+
+    image_paths = []
+    image_with_rect = None
+
+    paper_error = []
+    font_style_error = []
+    font_size_error  = []
+    font_color_error = []
+    margin_error     = []
+    spacing_error    = []
+
+    errors = {
+        'paper_size' : paper_error,
+        'font_family' : font_style_error,
+        'font_size' : font_size_error,
+        'font_color' : font_color_error,
+        'margins' : margin_error,
+        'spacings' : spacing_error
+    }
+
+    increment_page = 0
+
+    if starting_page == 0:
+            # add more functions to this
+            paper_error.append('No pages detected.')
+
+    for page_num in range(starting_page - 1, ending_page):
+        page = pdf_document[page_num]
+        pix = page.get_pixmap(matrix=fitz.Matrix(px, px))
+        image_np = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.height, pix.width, 3)
+        image_with_rect = image_np.copy()
+
+        # Get BBox from image to text and GET text
+        tesseract_data = pytesseract.image_to_data(image_np, output_type=pytesseract.Output.DICT)
+        new_data = remove_rn(tesseract_data)
+        n_boxes = len(new_data['text'])
+        
+        # Font Analysis
+        blocks = page.get_text("dict")['blocks']
+        width = page.get_text("dict")['width']
+        height = page.get_text("dict")['height']
+
+        # Check paper size
+        # A Letter Size = 612 × 792
+        if height != 792 or width != 612: # EDIT MAMAHYA
+            paper_error.append(increment_page)
+
+        # === Font ===
+        for block in blocks:    
+            if 'image' not in block: # analyze texts only
+                lines = block['lines']
+                for line in lines:
+                    spans = line['spans']
+                    for span in spans:
+                        if span['text'] != ' ': # dont include empty chars
+                            size = span['size']
+                            color = span['color']
+                            fonts = span['font']
+                            overlay = image_with_rect.copy()
+
+                            # This part will cluster the data and append the errors
+                            if fonts not in acceptable_fonts:
+                                cv2.rectangle(overlay, (int(span['bbox'][0]) * px, int(span['bbox'][1]) * px),
+                                            (int(span['bbox'][2]) * px, int(span['bbox'][3]) * px), (62,199,214), -1)
+                                cv2.addWeighted(overlay, 0.3, image_with_rect, 1 - 0.3, 0, image_with_rect)
+                                # append to list
+                                font_style_error.append({"page": increment_page, "fonts": fonts})
+
+                            if size not in {12.0}: # size 12
+                                cv2.rectangle(overlay, (int(span['bbox'][0]) * px, int(span['bbox'][1]) * px),
+                                            (int(span['bbox'][2]) * px, int(span['bbox'][3]) * px), (241,232,38), -1)
+                                cv2.addWeighted(overlay, 0.3, image_with_rect, 1 - 0.3, 0, image_with_rect)
+                                # append to list
+                                font_size_error.append({"page": increment_page, "size": size})
+
+                            if color not in {0}: # not black
+                                cv2.rectangle(overlay, (int(span['bbox'][0]) * px, int(span['bbox'][1]) * px),
+                                                (int(span['bbox'][2]) * px, int(span['bbox'][3]) * px), (241,75,38), -1)
+                                cv2.addWeighted(overlay, 0.3, image_with_rect, 1 - 0.3, 0, image_with_rect)
+                                # append to list
+                                font_color_error.append({"page": increment_page, "color": color})
+
+        # collect text boxes
+        text_boxes = []
+        for i in range(n_boxes):    
+            if i != 0:
+                x, y, w, h = new_data['left'][i], new_data['top'][i], new_data['width'][i], new_data['height'][i]
+                text_boxes.append((x, y, w, h))
+
+        # calculate margins and spacings with parameters: IMAGE and TEXT BOXES 
+        top_margin, bottom_margin, left_margin, right_margin, min_x, max_x, spacings, spacing_idx = calculate(image_with_rect, text_boxes)
+        # cv2.cvtColor(image_with_rect, cv2.COLOR_RGB2BGR)
+
+        # draw margins and spacing on the image
+        margin_ret = margin(image_with_rect, top_margin, bottom_margin, left_margin, right_margin, margins_json)
+        margin_error.append({"page": increment_page, "margins_arr": margin_ret})
+        
+        spacings_ret = spacing(image_with_rect, spacings, spacing_idx, text_boxes, min_x, max_x, accepted_spacings)
+        spacing_error.append({"page": increment_page, "spacings_arr": spacings_ret})
+
+        image_path = f'{directory}/page_{increment_page}.png'
+
+        fig, ax = plt.subplots(figsize=(20, 25))
+
+        # remove image axis
+        ax.imshow(image_with_rect)
+        ax.axis('off')
+
+        fig.savefig(image_path, bbox_inches='tight', pad_inches=0)
+        plt.close(fig)
+
+        image_paths.append(image_path)
+
+        # BREAK THE LOOP WHEN THE PAGE EQUAL TO 40
+        # MAX PAGE: 25
+        if increment_page + 1 == 25:
+            break
+
+        increment_page += 1
+        
+    return image_paths, errors
+
+def cluster_errors(errors):
+    grouped_data = {}
+
+    for item in errors.get('font_size', []):
+        page = item.get('page')
+        size = int(item.get('size'))
+        
+        # Find the corresponding font families for the page
+        families = [f_item.get('fonts') for f_item in errors.get('font_family', []) if f_item.get('page') == page]
+        unique_families = set(families) - set(grouped_data.get(page, {'font_family': []})['font_family']) # remove this if error
+        
+        if page not in grouped_data:
+            grouped_data[page] = {
+                'font_size': [size],
+                'font_family': list(unique_families) if unique_families else [],
+                'font_color': [],
+                'spacings_arr': [],
+                'margins_arr': []
+            }
+        else:
+            grouped_data[page]['font_size'].append(size)
+            grouped_data[page]['font_family'].extend(list(unique_families))
+
+    # iterate through the 'font_color' data and add it to the corresponding 'page'
+    for item in errors.get('font_color', []):
+        page = item.get('page')
+        color = item.get('color')
+
+        if page in grouped_data:
+            grouped_data[page]['font_color'].append(color)
+
+    # iterate through the 'spacings' data and add it to the corresponding 'page'
+    for item in errors.get('spacings', []):
+        page = item.get('page')
+        spacings = item.get('spacings_arr')
+
+        if page in grouped_data:
+            grouped_data[page]['spacings_arr'].extend(spacings)
+        else:
+            grouped_data[page] = {
+                'font_size': [],
+                'font_family': [],
+                'font_color': [],
+                'spacings_arr': spacings,  # Initialize spacings_arr with spacings
+                'margins_arr': []
+            }
+
+    # iterate through the 'margins' data and add it to the corresponding 'page'
+    for item in errors.get('margins', []):
+        page = item.get('page')
+        margins = item.get('margins_arr')
+
+        if page in grouped_data:
+            grouped_data[page]['margins_arr'] = margins
+
+    result = {page: data for page, data in grouped_data.items()}
+
+    return result
+
---
+++ main.py
@@ -0,0 +1,60 @@
+import uvicorn
+from fastapi import FastAPI, UploadFile, Form
+import os
+import base64
+from checker import getFontStyle, analyzePDF, cluster_errors
+
+# Define folder names
+folders_to_create = ["temp", "temp_images", "temp_images/Preliminaries/", "temp_images/Chapter 1/", "temp_images/Chapter 2/", "temp_images/Chapter 3/", "temp_images/Chapter 4/", "temp_images/Chapter 5/", "temp_images/Bibliography/"]
+# Loop through the list of folders and create them if they don't exist
+for folder_name in folders_to_create:
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        print(f"Created folder: {folder_name}")
+    else:
+        print(f"Folder '{folder_name}' already exists.")
+
+app = FastAPI()
+
+@app.post("/upload/")
+async def analyze_pdf(pdf_file: UploadFile, selection: str = Form(...)):
+    # print(selection)
+    # Check if a file was uploaded
+    if pdf_file is None:
+        return {"Error": 'No PDF file is uploaded.'}
+
+    # Check if the file has a PDF file extension
+    if not pdf_file.filename.endswith(".pdf"):
+        return {"Error": 'The uploaded file is not a PDF.'}
+
+
+    # Save the uploaded PDF file to a temporary location
+    file_path = os.path.join("temp", pdf_file.filename)
+    with open(file_path, "wb") as temp_file:
+        temp_file.write(pdf_file.file.read())
+
+    # Check and GET DATABASE values 
+    font_family, spacings_result, margins = 'Times New Roman', '2', {'margin_top': 1, 'margin_bottom': 1, 'margin_left': 1.5, 'margin_right': 1}
+    font_family = getFontStyle(font_family)
+    pdf_path = F'C:\\Users\\arola\OneDrive\\Desktop\\Code\\Thesis 1\\temp\\{pdf_file.filename}'
+
+    # Run font detection on the uploaded PDF file to get a list of image paths
+    image_paths, errors = analyzePDF(pdf_path, font_family, spacings_result, margins, selection)
+
+    if isinstance(image_paths, str):
+        return f"Error: {image_paths}"
+
+    result = cluster_errors(errors)
+
+    image_data_list = []  # list to store base64-encoded image data for each page
+
+    for image_path in image_paths:
+        with open(image_path, "rb") as image_file:
+            image_data = base64.b64encode(image_file.read()).decode("utf-8")
+            image_data_list.append(image_data)
+
+    # Return the image data as JSON response
+    return {"image_data_list": image_data_list, "errors" : result}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
